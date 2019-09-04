159581480873fb3531160e75b1460ce586e374e0
Sergii Khromchenko <sergii.khromchenko@verizondigitalmedia.com>
2019-09-03 18:22:56 -0700

    beta

---
+++ assets/js/override.js
@@ -0,0 +1,7 @@
+// var $x = jQuery.noConflict();
+//    alert("Version: "+$x.fn.jquery);
+   
+// src="{{site.baseurl}}/assets/js/jquery-3.4.1.min.js"
+$j(window).on('load', function () {
+    $j("#container1").twentytwenty();
+});
